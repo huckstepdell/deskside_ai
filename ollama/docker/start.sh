c04e788e473703a778f3de7927e838c378a2a766
Colin Huckstep <colin@huckstep.org>
2026-09-06 14:01:43 -0400

    bind to all

--- ollama/docker/start.sh
+++ ollama/docker/start.sh
@@ -1,5 +1,5 @@
-export OLLAMA_BIND_IP=100.94.55.35
 export PULL_MODELS="qwen3-coder-next:q4_K_M devstral-small-2:24b gpt-oss:latest"
 mkdir -p "/container_data/ollama/ollama-data"
 mkdir -p "/container_data/open-webui/open-webui-data"
+docker compose pull
 docker compose up -d
--- ollama/docker/stop.sh
+++ ollama/docker/stop.sh
@@ -1,3 +1,2 @@
-export OLLAMA_BIND_IP=100.94.55.35
 export PULL_MODELS="qwen3-coder-next:q4_K_M devstral-small-2:24b"
 docker compose down
